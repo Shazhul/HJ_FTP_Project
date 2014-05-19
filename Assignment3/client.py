bcf37f7c24e5f54b8a71a99a4a5508237ddfc5e8
Jason Tupper <JasonETupper@gmail.com>
2014-05-18 17:35:43 -0700

    LS is working!

--- Assignment3/client.py
+++ Assignment3/client.py
@@ -7,31 +7,71 @@
 import os
 import sys
 import socket
-from cmds import FTP_COMMANDS
 
 # Command line checks 
 if len(sys.argv) < 3:
 	print "USAGE python " + sys.argv[0] + " <SERVER_MACHINE> <SERVER_PORT>"
 	exit(0)
 
+def ls(ephyconn):
+	fileListSize = unpad(recvAll(ephyconn, 1024))
+	ephyconn.sendall('1')
+	fileList = recvAll(ephyconn, (int(fileListSize)-1))
+	ephyconn.sendall('1')
+	print(fileList)
+
+def pad(s, length):		
+	padding = '&'
+	s = str(s)
+	if len(s) < length:
+		s = s+ (length - len(s))*padding
+	return s
+
+def unpad(s):
+    padding = '&'
+    return s.strip(padding)
+
+	#Get the list of files
+def setupEphyConn(startSock):
+	ephySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	ephySock.bind(('localhost',0))
+	ephySock.listen(1)
+	ephyPort = ephySock.getsockname()[1]
+	#Sending 5 bytes
+	startSock.sendall(pad(ephyPort, 5))
+	success = recvAll(startSock, 1)
+	ephyconn, ephyaddress = ephySock.accept()
+	return ephyconn
+
+def recvAll(sock, numBytes):
+    # The buffer
+    recvBuff = ""
+    # The temporary buffer
+    tmpBuff = ""
+    # Keep receiving till all is received
+    while len(recvBuff) < int(numBytes):
+        # Attempt to receive bytes
+        tmpBuff =  sock.recv(int(numBytes))
+        # The other side has closed the socket
+        if not tmpBuff:
+            break
+        # Add the received bytes to the buffer
+        recvBuff += tmpBuff
+    return recvBuff
+#____________________________START___________________
+
 # Server address
 serverAddr = socket.gethostbyname(sys.argv[1])
-
 # Server port
 serverPort = int(sys.argv[2])
-
 # Create a command TCP socket
 commandSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 # Connect both sockets to the server
 try:
 	commandSock.connect((serverAddr, serverPort))
 except:
 	print("Could not connect")
 	exit(0)
-
-# The number of bytes sent
-numSent = 0
 
 # Keep sending until all is sent
 while True:
@@ -41,12 +81,22 @@ while True:
 		print('Exiting')
 		break
 
-	newCmd = FTP_COMMANDS(cmd)
+	if(cmd == 'ls'):
+		#Send the command size - 1 byte
+		commandSock.sendall('2')
+		success = recvAll(commandSock, 1)
+		#Send the command - cmdsize bytes
+		commandSock.sendall(cmd)
+		success = recvAll(commandSock, 1)
+		ephyconn = setupEphyConn(commandSock)
+		ls(ephyconn)
+		ephyconn.close()
 
-	if newCmd.isCommand:
-		#negotiate ephy port
-		#create ephy socket
-		newCmd.RunCommand(ephySock) #on ephy socket
-		#close ephy socket
-	
+	if(cmd == 'get'):
+		pass#getfile()
+
+	if(cmd == 'put'):
+		pass#putfile()
+
 commandSock.close()
+
--- Assignment3/oldserver.py
+++ Assignment3/oldserver.py
@@ -6,8 +6,6 @@
 
 import sys
 import socket
-from os import listdir
-from os.path import isfile
 from cmds import FTP_COMMANDS
 
 # Command line checks 
@@ -15,40 +13,14 @@ if len(sys.argv) < 2:
 	print "USAGE python " + sys.argv[0] + " <SERVER_PORT>" 
 	exit(0)
 
-
-def recvEphyConn(startSock):
-	ephyPort = unpad(recvAll(startSock, 5))
-	clientSock.sendall('1')
-	ephySock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	ephySock.connect(('localhost', int(ephyPort)))
-	return ephySock
-
-def pad(s, length):
-    padding = '&'
-    s = str(s)
-    if len(s) < length:
-        s = s+ (length - len(s))*padding
-    return s
-
-def unpad(s):
-	padding = '&'
-	return s.strip(padding)
-
-def GetListInDirectory():
-	p = listdir('.')
-	s = ''
-	for f in p:
-		s = s + f + '\n'
-	return s
-
-#_____________________START____________
-
-
 welcomePort = int(sys.argv[1])
+
 # Create a welcome socket. 
 welcomeSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 # Bind the socket to the port
 welcomeSock.bind(('', welcomePort))
+
 # Start listening on the socket
 welcomeSock.listen(10)
 
@@ -60,29 +32,28 @@ welcomeSock.listen(10)
 # @return - the bytes received
 # *************************************************
 def recvAll(sock, numBytes):
+
 	# The buffer
 	recvBuff = ""
+	
 	# The temporary buffer
 	tmpBuff = ""
+	
 	# Keep receiving till all is received
 	while len(recvBuff) < int(numBytes):
+		
 		# Attempt to receive bytes
-		tmpBuff =  sock.recv(int(numBytes))
+		tmpBuff =  sock.recv(numBytes)
+		
 		# The other side has closed the socket
 		if not tmpBuff:
 			break
+		
 		# Add the received bytes to the buffer
 		recvBuff += tmpBuff
+	
 	return recvBuff
-
-def ls(ephyconn):
-	fileList = GetListInDirectory()
-	fileListSize = len(fileList)
-	ephyconn.sendall(pad(fileListSize, 1024))
-	recvAll(ephyconn, 1)
-	ephyconn.sendall(fileList, fileListSize)
-	recvAll(ephyconn, 1)
-
+		
 # Accept connections forever
 while True:
 	
@@ -91,19 +62,18 @@ while True:
 	clientSock, addr = welcomeSock.accept()
 	print "Accepted connection from client: ", addr, "\n"
 	
-	while 1:
-		# The size of the incoming command
-		cmdSize = recvAll(clientSock, 1)
-		clientSock.sendall('1')
-		# Get the inc. command
-		cmd = recvAll(clientSock, cmdSize)
-		clientSock.sendall('1')
-		if(cmd == 'ls'):
-			# Get the ephy port
-			ephyconn = recvEphyConn(clientSock)
-			ls(ephyconn)
-			ephyconn.close()
+	# The size of the incoming command
+	cmdSize = recvAll(clientSock, 4)
+	#get the size of the inc command	
 
+	# Receive the first 10 bytes indicating the
+	# size of the command
+	cmdBuff = recvAll(clientSock, cmdSize)
+	serverCmd = FTP_COMMANDS(cmdBuff)
+	
+	if serverCmd.isCommand:
+		serverCmd.RunCommand(ftpconnection)
+		
 # Close our side
 clientSock.close()
-
+	
